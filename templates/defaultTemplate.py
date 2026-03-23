67f2087831b3e184bfc879db8e2fa4f55e1a6224
davidleonstr <davidalfonsoleoncarmona@gmail.com>
2026-03-22 23:25:55 -0600

    When the application is minimized, it remains hide in the background

--- templates/defaultTemplate.py
+++ templates/defaultTemplate.py
@@ -1,15 +1,31 @@
 from QFlow import Template
+from QFlow.modules import config
+
 from QFlow.components import TitleBar
-from qtpy.QtWidgets import QVBoxLayout, QStackedWidget, QSizeGrip
-from qtpy.QtCore import Qt
+from qtpy.QtWidgets import QVBoxLayout, QStackedWidget, QSizeGrip, QApplication, QSystemTrayIcon, QMenu
+from qtpy.QtCore import Qt, QTimer
+
+from qtpy.QtGui import QIcon, QAction
+
+from config import CONFIG
+from helpers import JSONFile, ObjectBuilder
+
+SCREENCONFIG = ObjectBuilder(
+    JSONFile(CONFIG.folders['configs']['screens']['template']).read()
+).obj
 
 # Default window template
+@config(SCREENCONFIG)
 class DefaultTemplate(Template):
     def __init__(self, parent) -> None:
         super().__init__(parent)
 
         # Window bar
-        self.titleBar = TitleBar(parent=parent, title=self.parent().title)
+        self.titleBar = TitleBar(
+            parent=parent, 
+            title=self.parent().title, 
+            onWindowMinimize=self.windowsMinimize,
+        )
 
         # Main Layout
         self.mainLayout = QVBoxLayout()
@@ -33,3 +49,56 @@ class DefaultTemplate(Template):
 
         # Set layout
         self.setLayout(self.mainLayout)
+
+        # Set tray
+        self.trayIcon = QSystemTrayIcon(self)
+        self.trayIcon.setIcon(QIcon(self.Config.icons.appIcon)) # Icon
+
+        # Create tray menu
+        trayMenu = QMenu()
+
+        # Add actions
+        showAction = QAction(self.Config.texts.open, self)
+        quitAction = QAction(self.Config.texts.close, self)
+
+        # Set actions
+        showAction.triggered.connect(self.showWindow)
+        quitAction.triggered.connect(QApplication.quit)
+
+        # Add actions
+        trayMenu.addAction(showAction)
+        trayMenu.addAction(quitAction)
+
+        # Set context menu to the icon
+        self.trayIcon.setContextMenu(trayMenu)
+
+        # When shows options
+        self.trayIcon.activated.connect(self.onTrayClick)
+
+        # Show tray icon
+        self.trayIcon.show()
+    
+    # When the window is minimized
+    def windowsMinimize(self):
+        # Change order
+        QTimer.singleShot(0, self.parent().hide)
+
+        self.trayIcon.showMessage(
+            self.Config.texts.minimized,
+            self.Config.texts.whenItIsMinimized,
+            QSystemTrayIcon.MessageIcon.Information,
+            1000
+        )
+
+    # When the windows is showed
+    def showWindow(self):
+        # RANDOM BULLSHIT GO !!!
+        self.parent().show()
+        self.parent().setWindowState(Qt.WindowState.WindowNoState)
+        self.parent().activateWindow()
+        self.parent().showNormal()
+
+    # I don't know. I found this in stackoverflow
+    def onTrayClick(self, reason):
+        if reason == QSystemTrayIcon.ActivationReason.Trigger:
+            self.showWindow()
